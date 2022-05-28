4d96ad492ec9a24f43e53b46a377e254a524000e
Jan Spennemann <janpeter.spennemann@gmail.com>
2022-05-28 20:11:07 +0200

    Added collisions to Player

--- Player.gd
+++ Player.gd
@@ -5,11 +5,26 @@ var screen_size
 
 onready var _animation_player = $AnimationPlayer
 
+onready var _collision_idle_right = $CollisionIdleRight
+onready var _collision_idle_left = $CollisionIdleLeft
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_animation_player.play("idle_right")
+	set_animation("idle_left")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func disable_collisions():
+	_collision_idle_right.disabled = true
+	_collision_idle_left.disabled = true
+
+func set_animation(name):
+	if name == "idle_right":
+		disable_collisions()
+		_animation_player.play("idle_right")
+	elif name == "idle_left":
+		disable_collisions()
+		_animation_player.play("idle_left")

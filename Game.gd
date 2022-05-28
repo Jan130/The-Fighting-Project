0a544d07a3e93dc94634d2be68907d17b62323b4
Jan Spennemann <janpeter.spennemann@gmail.com>
2022-05-28 20:56:41 +0200

    Implemented basic player movement

---
+++ Game.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+onready var player = $Player
+
+var screen_touched = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		if event.is_pressed():
+			screen_touched = true
+		else:
+			screen_touched = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var player_velocity = Vector2.ZERO
+	if screen_touched:
+		player_velocity.x += 1
+	player.move(player_velocity, delta)
--- Player.gd
+++ Player.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 export var speed = 400
+
 var screen_size
+var previous_velocity
 
 onready var _animation_player = $AnimationPlayer
 
@@ -10,8 +12,9 @@ onready var _collision_idle_left = $CollisionIdleLeft
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_animation("idle_left")
-
+	screen_size = get_viewport_rect().size
+	previous_velocity = Vector2.ZERO
+	set_animation("idle_right")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -21,10 +24,24 @@ func disable_collisions():
 	_collision_idle_right.disabled = true
 	_collision_idle_left.disabled = true
 
-func set_animation(name):
+func set_animation(name : String):
 	if name == "idle_right":
 		disable_collisions()
 		_animation_player.play("idle_right")
 	elif name == "idle_left":
 		disable_collisions()
 		_animation_player.play("idle_left")
+
+func move(velocity : Vector2, delta):
+	if velocity != previous_velocity:
+		if velocity.x == 0:
+			pass
+		elif velocity.x > 0:
+			set_animation("idle_right")
+		else:
+			set_animation("idle_left")
+		previous_velocity = velocity
+	
+	position += velocity * delta * speed
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)

8f006f8ed7996b5b1c5d26a37cd9d24265fc610c
Jan Spennemann <janpeter.spennemann@gmail.com>
2022-05-29 11:06:30 +0200

    Removed buttons for control

--- Game.gd
+++ Game.gd
@@ -2,24 +2,30 @@ extends Node2D
 
 onready var player = $Player
 
+var screen_size
+
 var screen_touched = false
+var lastTouch
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 func _input(event):
 	if event is InputEventScreenTouch:
 		if event.is_pressed():
 			screen_touched = true
+			lastTouch = event.position
 		else:
 			screen_touched = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var player_velocity = Vector2.ZERO
-	if Input.is_action_pressed("UiRight"):
-		player_velocity.x += 1
-	if Input.is_action_pressed("UiLeft"):
-		player_velocity.x -= 1
+	
+	if screen_touched:
+		if lastTouch.x > screen_size.x / 2:
+			player_velocity.x += 1
+		else:
+			player_velocity.x -= 1
 	player.move(player_velocity, delta)
